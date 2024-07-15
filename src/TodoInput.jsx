8d455b6f7d910b4d52b668b2064ba5a10e5753f0
Aldrin Bright <aldrinbright@gmail.com>
2024-07-16 04:29:07 +0530

    update

--- src/TodoInput.jsx
+++ src/TodoInput.jsx
@@ -2,54 +2,43 @@ import axios from "axios";
 import React, { useState } from "react";
 
 function TodoInput() {
+  let [input, setInput] = useState({
+    title: "",
+    id: "",
+    isChecked: false,
+  });
 
-
-   let [input, setInput] = useState({
-    title: '',
-    id: '',
-    isChecked: false
-
-   });
-
-
-
-
-
-    function handleSubmit (e){
-
-        e.preventDefault();
-        axios.post('http://localhost:3000/todoList', input)
-        .then(res => {
-            console.log(res);
-            setInput({
-                title: '',
-                id: '',
-                isChecked: false
-               
-              });
-        })
-        
-    }
-
-
+  function handleSubmit(e) {
+    e.preventDefault();
+    axios.post("http://localhost:3000/todoList", input).then((res) => {
+      setInput({
+        title: "",
+        id: Math.random().toString(36).substr(2, 9),
+        isChecked: false,
+      });
+    });
+  }
 
   return (
     <div className="todoInput">
+      <form action="" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Enter the Task"
+          value={input.title}
+          onChange={(e) =>
+            setInput({
+              ...input,
+              title: e.target.value,
+              id: Math.random().toString(36).substr(2, 9),
+              isChecked: false,
+            })
+          }
+        />
 
-    <form action="" onSubmit={handleSubmit}>
-
-
- 
-
-      <input type="text" 
-        placeholder= 'Enter the Task'
-      
-      onChange={(e) => setInput({...input,title : e.target.value, id: Math.random().toString(36).substr(2, 9), isChecked: false})}
-      />
-
-      <button type="submit" className="todoSubmit">
-        Add Task
-      </button>
+        <button type="submit" className="todoSubmit">
+          Add Task
+        </button>
       </form>
     </div>
   );
